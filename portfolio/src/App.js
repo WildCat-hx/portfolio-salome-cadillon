208ed5a62d542e699a96dd29f6698368bbb69553
Salomé Cadillon <60703994+WildCat-hx@users.noreply.github.com>
2024-01-23 17:12:14 +0100

    dev : import et injection des composants dans app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import './styles/global.css';
+import React from 'react';
+import { NavComponent } from './components/navigation/navbar.js';
+import { ScrollComponent } from './components/scroll-top/scroll-top.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <NavComponent/>
+      </header>
+      <main>
+        
+      </main>
+        <ScrollComponent/>
+      <footer>     
+      </footer>
     </div>
   );
 }
+
+
 
 export default App;
